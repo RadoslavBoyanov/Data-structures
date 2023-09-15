2551115d5bbe83fa3309d6bdf31fb77ec4436f64
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2023-09-15 22:29:46 +0300

    DoublyLinkedList Implementation

---
+++ Linear Data Structures/DoublyLinkedList/02.DoublyLinkedList/DoublyLinkedList.cs
@@ -0,0 +1,90 @@
+﻿namespace Problem02.DoublyLinkedList
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class DoublyLinkedList<T> : IAbstractLinkedList<T>
+    {
+
+        private class Node
+        {
+            public Node Next { get; set; }
+            public Node Previous { get; set; }
+            public T Value { get; set; }
+
+            public Node(T value)
+            {
+                this.Value = value;
+            }
+        }
+
+        private Node head;
+        private Node tail;
+
+        public int Count { get; private set; }
+
+        public void AddFirst(T item)
+        {
+            Node node = new Node(item);
+            if (this.head == null)
+            {
+                this.head = node;
+                this.tail = node;
+            }
+            else
+            {
+                this.head.Previous = node;
+                node.Next = this.head;
+                this.head = node;
+                Count++;
+            }
+        }
+
+        public void AddLast(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetFirst()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetLast()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T RemoveFirst()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T RemoveLast()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            Node currentNode = this.head;
+
+            while (currentNode != null)
+            {
+                yield return currentNode.Value;
+                currentNode = currentNode.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+
+        private void CheckIfDoblyLinkedListIsEmpty()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("Dobly Linked List is empty!");
+            }
+        }
+    }
+}
