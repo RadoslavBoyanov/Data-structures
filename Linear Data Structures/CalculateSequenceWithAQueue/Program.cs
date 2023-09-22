597a576517712e4e610fb9dfc0fc2a2e2ba904b5
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2023-09-22 14:50:10 +0300

    Linear Data Structures Exercise

---
+++ Linear Data Structures/CalculateSequenceWithAQueue/Program.cs
@@ -0,0 +1,29 @@
+﻿using Problem01.CircularQueue;
+
+namespace _06.CalculateSequenceWithAQueue
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int current = int.Parse(Console.ReadLine());
+
+            var queue = new Queue<int>();
+            queue.Enqueue(current);
+
+            for (int i = 0; i < 50; i++)
+            {
+                var s1 = current + 1;
+                var s2 = 2 * current + 1;
+                var s3 = current + 2;
+
+                queue.Enqueue(s1);
+                queue.Enqueue(s2);
+                queue.Enqueue(s3);
+                current = queue.ElementAtOrDefault(i);
+            }
+
+            Console.WriteLine(string.Join(" ", queue.ToArray()));
+        }
+    }
+}
---
+++ Linear Data Structures/ReverseNumberswithAStack/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _05.ReverseNumberswithAStack
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            var stack = new Stack<int>();
+
+            foreach (var number in input)
+            {
+                stack.Push(number);
+            }
+
+            Console.WriteLine(string.Join(" ", stack));
+
+        }
+    }
+}
