85cf4070b34db4b1dbf9da8be7b7cac9706418a8
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2023-10-02 10:55:42 +0300

    BTS and Heaps Exercise

---
+++ Binary tree search and Heaps/Exercise/04.CookiesProblem/CookiesProblem.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using _03.MinHeap;
+using Wintellect.PowerCollections;
+
+namespace _04.CookiesProblem
+{
+    public class CookiesProblem
+    {
+        public int Solve(int minSweetness, int[] cookies)
+        {
+            var priorityQueue = new OrderedBag<int>();
+
+            foreach (var cookie in cookies)
+            {
+                priorityQueue.Add(cookie);
+            }
+
+            int currentSweetnes = priorityQueue[0];
+
+            int numberOfOperations = 0;
+
+            while (currentSweetnes > minSweetness && priorityQueue.Count > 0) 
+            {
+                int leastSweetCookie = priorityQueue.RemoveFirst();
+                int secondLeastSweetCookie = priorityQueue.RemoveFirst();
+
+                int specialCombinedCookie = leastSweetCookie + 2 * secondLeastSweetCookie;
+
+                priorityQueue.Add(specialCombinedCookie);
+                
+                currentSweetnes= priorityQueue[0];
+
+                numberOfOperations++;
+            }
+
+            if (currentSweetnes < minSweetness)
+            {
+                return -1;
+            }
+            return numberOfOperations;
+        }
+    }
+}
---
+++ Binary tree search and Heaps/Exercise/Playground/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using _02.BinarySearchTree;
+using _03.MinHeap;
+using _04.CookiesProblem;
+
+namespace Playground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //BinarySearchTree<int> bst = new BinarySearchTree<int>();
+
+            //bst.Insert(1);
+            //bst.Insert(3);
+            //bst.Insert(4);
+            //bst.Insert(5);
+            //bst.Insert(8);
+            //bst.Insert(9);
+            //bst.Insert(10);
+            //bst.Insert(37);
+            //bst.Insert(39);
+            //bst.Insert(45);
+
+            //bst.Delete(37);
+
+            //bst.EachInOrder(Console.WriteLine);
+
+            int requiredSweetnest = int.Parse(Console.ReadLine());
+
+            int[] cookies = 
+        }
+    }
+}
