d9976a0930bfd27d319ad9fb40b6112f76c554df
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2023-10-09 13:02:09 +0300

    Exam preparation

---
+++ ExamPreparation/Dec-2022-Exam/Exam.MovieDatabase/Actor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace Exam.MovieDatabase
+{
+    public class Actor
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public ICollection<Movie> Movies { get; set; }
+
+        public Actor(string id, string name, int age)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+
+            this.Movies = new HashSet<Movie>(); 
+        }
+    }
+}
---
+++ ExamPreparation/Dec-2022-Exam/Exam.MovieDatabase/Movie.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace Exam.MovieDatabase
+{
+    public class Movie
+    {
+        public string Id { get; set; }
+
+        public int DurationInMinutes { get; set; }
+
+        public string Title { get; set; }
+
+        public double Rating { get; set; }
+
+        public double Budget { get; set; }
+
+        public ICollection<Actor> Actors { get; set; }
+
+        public Movie(string id, int durationInMinutes, string title, double rating, double budget)
+        {
+            Id = id;
+            DurationInMinutes = durationInMinutes;
+            Title = title;
+            Rating = rating;
+            Budget = budget;
+            this.Actors = new List<Actor>();    
+        }
+    }
+}
