72ef3efe4755a688df027134e1286c8dbe2b7642
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2023-10-16 20:53:51 +0300

    Exam preparation

---
+++ ExamPreparation/March-26-2022-Exam/01. Barber Shop_Skeleton/Barber.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace BarberShop
+{
+    public class Barber
+    {
+        public Barber(string name, int haircutPrice, int stars)
+        {
+            this.Name = name;
+            this.HaircutPrice = haircutPrice;
+            this.Stars = stars;
+            this.Clients= new List<Client>();
+        }
+
+        public string Name { get; set; }
+        public int HaircutPrice { get; set; }
+        public int Stars { get; set; }
+        public ICollection<Client> Clients { get; set; }
+    }
+}
---
+++ ExamPreparation/March-26-2022-Exam/03. Trip Administrator_Skeleton/Company.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TripAdministrations
+{
+    public class Company
+    {
+        public Company(string name, int tripOrganizationLimit)
+        {
+            this.Name = name;
+            this.TripOrganizationLimit = tripOrganizationLimit;
+            this.Trips = new List<Trip>();  
+        }
+
+        public string Name { get; set; }
+
+        public int TripOrganizationLimit { get; set; }
+
+        public int CurrentTrips { get; set; }
+        public ICollection<Trip> Trips { get; set; }
+    }
+}
