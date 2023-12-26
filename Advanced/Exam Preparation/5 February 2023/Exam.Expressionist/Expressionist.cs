97c0f977da889a802c249d468d3f49f8b4577c92
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2023-12-26 18:44:34 +0200

    Exam preparation

---
+++ Advanced/Exam Preparation/5 February 2023/Exam.Expressionist/Expressionist.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace Exam.Expressionist
+{
+    public class Expressionist : IExpressionist
+    {
+        private Dictionary<string, Expression> expressionsById;
+        private Expression root;
+        
+        public Expressionist()
+        {
+            this.expressionsById = new Dictionary<string, Expression>();
+        }
+
+        public void AddExpression(Expression expression)
+        {
+            if (root != null)
+            {
+                throw new ArgumentException();
+            }
+
+            this.root = expression;
+
+            this.BfsAddToExpressionsCollection(expression);
+        }
+
+        public void AddExpression(Expression expression, string parentId)
+        {
+            if (!this.expressionsById.ContainsKey(parentId))
+            {
+                throw new ArgumentException();
+            }
+
+            var parent = this.expressionsById[parentId];
+
+            if (parent.LeftChild != null && parent.RightChild != null)
+            {
+                throw new ArgumentException();
+            }
+
+            if (parent.LeftChild is null)
+            {
+                parent.LeftChild = expression;
+            }
+            else if (parent.LeftChild != null && parent.RightChild is null)
+            {
+                parent.RightChild = expression;
+            }
+
+            expression.Parent = parent;
+
+            this.BfsAddToExpressionsCollection(expression);
+        }
+
+        public bool Contains(Expression expression) => this.expressionsById.ContainsKey(expression.Id);
+
+        public int Count() => this.expressionsById.Count;
+
+        public Expression GetExpression(string expressionId)
+        {
+            if (!this.expressionsById.ContainsKey(expressionId))
+            {
+                throw new ArgumentException();
+            }
+
+            return this.expressionsById[expressionId];
+        }
+
+        public void RemoveExpression(string expressionId)
+        {
+            if (!this.expressionsById.ContainsKey(expressionId))
+            {
+                throw new ArgumentException();
+            }
+
+            var expression = this.expressionsById[expressionId];
+
+            if (expression.Parent is null)
+            {
+                root = null;
+                this.expressionsById.Remove(expressionId);
+            }
+
+            var parent = this.expressionsById[expression.Parent.Id];
+
+            if (parent.LeftChild == expression)
+            {
+                parent.LeftChild = parent.RightChild;
+            }
+
+            parent.RightChild = null;
+
+            this.Bfs(expression, e => this.expressionsById.Remove(e.Id));
+        }
+
+        public string Evaluate() => this.Evaluate(this.root, "");
+
+        private string Evaluate(Expression expression, string result)
+        {
+            if (expression is null)
+            {
+                return result;
+            }
+
+            if (expression.Type == ExpressionType.Value)
+            {
+                result += expression.Value;
+            }
+            else
+            {
+                result +=
+                    $"({Evaluate(expression.LeftChild, "")} {expression.Value} {Evaluate(expression.RightChild, "")})";
+            }
+
+            return result;
+        }
+
+        //helper methods
+        private void BfsAddToExpressionsCollection(Expression expression)
+        {
+            var queue = new Queue<Expression>();
+            queue.Enqueue(expression);
+
+            while (queue.Count > 0)
+            {
+                var node = queue.Dequeue();
+                this.expressionsById.Add(node.Id, node);
+
+                if (node.LeftChild != null)
+                {
+                    queue.Enqueue(node.LeftChild);
+                    node.LeftChild.Parent = node;
+                }
+
+                if (node.RightChild != null)
+                {
+                    queue.Enqueue(node.RightChild);
+                    node.RightChild.Parent = node;
+                }
+            }
+        }
+
+        private void Bfs(Expression expression, Action<Expression> action)
+        {
+            var queue = new Queue<Expression>();
+            queue.Enqueue(expression);
+
+            while (queue.Count > 0)
+            {
+                var node = queue.Dequeue();
+                action(node);
+
+                if (node.LeftChild != null)
+                {
+                    queue.Enqueue(node.LeftChild);
+                }
+
+                if (node.RightChild != null)
+                {
+                    queue.Enqueue(node.RightChild);
+                }
+            }
+        }
+    }
+}
---
+++ Advanced/Exam Preparation/5 February 2023/Exam.PackageManagerLite/PackageManager.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exam.PackageManagerLite
+{
+    public class PackageManager : IPackageManager
+    {
+        private Dictionary<string, Package> packagesById;
+        private Dictionary<string, Dictionary<string, Package>> packagesByNameAndVersion;
+
+        public PackageManager()
+        {
+            this.packagesById = new Dictionary<string, Package>();
+            this.packagesByNameAndVersion = new Dictionary<string, Dictionary<string, Package>>();
+        }
+
+        public void RegisterPackage(Package package)
+        {
+            if (this.packagesByNameAndVersion.ContainsKey(package.Name) && this.packagesByNameAndVersion[package.Name].ContainsKey(package.Version))
+            {
+                throw new ArgumentException();
+            }
+            
+            this.packagesById.Add(package.Id, package);
+
+            if (!packagesByNameAndVersion.ContainsKey(package.Name))
+            {
+                this.packagesByNameAndVersion.Add(package.Name, new Dictionary<string, Package>());
+            }
+
+            this.packagesByNameAndVersion[package.Name].Add(package.Version, package);
+        }
+
+        public void RemovePackage(string packageId)
+        {
+            if (!this.packagesById.ContainsKey(packageId))
+            {
+                throw new ArgumentException();
+            }
+
+            var package = this.packagesById[packageId];
+            this.packagesByNameAndVersion[package.Name].Remove(package.Version);
+
+            if (this.packagesByNameAndVersion[package.Name].Count == 0)
+            {
+                this.packagesByNameAndVersion.Remove(package.Name);
+            }
+
+            this.packagesById.Remove(packageId);
+
+            foreach (var packageDependency in package.Dependants)
+            {
+                packageDependency.Dependencies.Remove(package);
+            }
+
+            foreach (var packageDependency in package.Dependencies)
+            {
+                packageDependency.Dependants.Remove(package);
+            }
+
+        }
+
+        public void AddDependency(string packageId, string dependencyId)
+        {
+            if (!this.packagesById.ContainsKey(packageId) || !this.packagesById.ContainsKey(dependencyId))
+            {
+                throw new ArgumentException();
+            }
+
+            var package = this.packagesById[packageId];
+            var dependecyPackage = this.packagesById[dependencyId];
+
+            package.Dependencies.Add(dependecyPackage);
+            dependecyPackage.Dependants.Add(package);
+        }
+
+        public bool Contains(Package package) => this.packagesById.ContainsKey(package.Id);
+
+        public int Count() => this.packagesById.Count;
+
+        public IEnumerable<Package> GetDependants(Package package)
+        {
+            if (!this.packagesById.ContainsKey(package.Id))
+            {
+                throw new ArgumentException();
+            }
+
+            return package.Dependants;
+        }
+
+        public IEnumerable<Package> GetIndependentPackages()
+            => this.packagesById.Values
+                .Where(p => p.Dependencies.Count == 0)
+                .OrderByDescending(p => p.ReleaseDate)
+                .ThenBy(p => p.Version);
+
+        public IEnumerable<Package> GetOrderedPackagesByReleaseDateThenByVersion()
+        {
+            var result = new List<Package>();
+            foreach (var packagesByVersion in packagesByNameAndVersion.Values)
+            {
+                var lastPackage = packagesByVersion.Values
+                    .OrderByDescending(p => p.ReleaseDate)
+                    .First();
+
+                result.Add(lastPackage);
+            }
+
+            return result.OrderByDescending(p => p.ReleaseDate)
+                .ThenBy(p => p.Version);
+        }
+    }
+}
